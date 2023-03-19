a0b2dbe703aa8fb38341a787515ab540beab7ba6
Thomas <sment1337@gmail.com>
2023-03-19 11:50:20 -0700

    minor changes

--- lnkScript.sh
+++ lnkScript.sh
@@ -20,7 +20,7 @@ sudo chmod -R 777 /usr/share/sddm/themes/Sugar-Candy/Backgrounds
 # Substituting Sugar Candy for sddm
 cat /usr/lib/sddm/sddm.conf.d/default.conf | sed 's/^Current=/Current=Sugar-Candy/g' | sudo tee /usr/lib/sddm/sddm.conf.d/default.conf
 # Substituting BG in sugar candy
-cat /usr/share/sddm/themes/Sugar-Candy/theme.conf | sed '/^Background/d' | sed '/General/a Background='\''Backgrounds/BG.jpg'\''' | sudo tee /usr/share/sddm/themes/Sugar-Candy/theme.conf.user
+cat /usr/share/sddm/themes/Sugar-Candy/theme.conf | sed '/^Background/d' | sed '/General/a Background='\"'Backgrounds/BG.jpg'\"'' | sudo tee /usr/share/sddm/themes/Sugar-Candy/theme.conf.user
 python .config/nitrogen/scraper.py
 sudo chown root:root /usr/share/sddm/themes/Sugar-Candy/Backgrounds/BG.jpg
 sudo chmod -R 777 /usr/share/sddm/themes/Sugar-Candy/Backgrounds/BG.jpg
