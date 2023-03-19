69dfaf55374271bd5a4c6100a6bd7b4c7ff65a9d
Thomas <sment1337@gmail.com>
2023-03-18 22:07:24 -0700

    added dot removal at the stat of the lnkScript

--- lnkScript.sh
+++ lnkScript.sh
@@ -1,3 +1,4 @@
+rm -rf .vim .zshrc .tmux.conf .config
 printf '1\nY\n' | sudo pacman -S terminator i3-wm i3status polybar upower python-pip base-devel wget sddm nitrogen
 sudo systemctl enable sddm.service
 git clone https://aur.archlinux.org/yay.git
@@ -6,11 +7,9 @@ printf 'Y\n' | makepkg -si
 cd ~
 printf 'Y\n' | yay -S ttf-jetbrains-mono-nerd autojump
 pip install bs4
-rm -rf .zshrc
 ln -s sdots/.vim ./.vim
 ln -s sdots/.zshrc ./.zshrc
 ln -s sdots/.tmux.conf ./.tmux.conf
-rm -rf .config/i3
 ln -s /home/sment/sdots/i3 .config/i3
 ln -s /home/sment/sdots/polybar .config/polybar
 ln -s /home/sment/sdots/nitrogen/ .config/nitrogen
