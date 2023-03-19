88789ea91d724538d64af7dd9f88b8731f1bc1b1
Thomas <sment1337@gmail.com>
2023-03-18 22:01:28 -0700

    automated the lnkScript

--- lnkScript.sh
+++ lnkScript.sh
@@ -1,12 +1,13 @@
-sudo pacman -S terminator i3-wm i3status polybar upower python-pip base-devel wget sddm nitrogen 
+printf '1\nY\n' | sudo pacman -S terminator i3-wm i3status polybar upower python-pip base-devel wget sddm nitrogen
 sudo systemctl enable sddm.service
 git clone https://aur.archlinux.org/yay.git
 cd yay
-makepkg -si
+printf 'Y\n' | makepkg -si
 cd ~
-yay -S ttf-jetbrains-mono-nerd autojump
+printf 'Y\n' | yay -S ttf-jetbrains-mono-nerd autojump
 pip install bs4
 rm -rf .zshrc
+ln -s sdots/.vim ./.vim
 ln -s sdots/.zshrc ./.zshrc
 ln -s sdots/.tmux.conf ./.tmux.conf
 rm -rf .config/i3
