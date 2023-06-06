26822114fe74acc2f57a5e726874cbeafbdc16d7
Thomas <sment1337@gmail.com>
2023-06-05 22:27:38 -0700

    Added file that opens pdfs instead of notes with zathura

---
+++ tmux/OpenPDF.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/zsh
+
+echo 'type search keyword or regexp:'
+
+read var1
+
+var2=$(pdfgrep --recursive --page-number $var1 "/home/sment/DripBox/iCloudDocs/Literature/spice manuals and Books" | fzf) #| cut -d":" -f1,2 
+
+path=$(echo $var2 | cut -d":" -f1)
+line=$(echo $var2 | cut -d":" -f2)
+
+Cmd="zathura -P "
+
+eval $Cmd $line \"$path\"
