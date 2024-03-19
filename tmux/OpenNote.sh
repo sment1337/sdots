c0a88a28ee1d8423e842cc8fb8be994d2cb3691f
Thomas <sment1337@gmail.com>
2024-03-19 09:15:15 -0700

    Added better OpenNote script using egrep

--- tmux/OpenNote.sh
+++ tmux/OpenNote.sh
@@ -4,11 +4,16 @@ echo 'type search keyword or regexp (/!\ Note: use -e ASDASD to do or search):'
 
 read var1
 
-var2=$(grep -rnw 'resilio/folders/DripBox/QownNotes' -e $var1 | fzf) #| cut -d":" -f1,2 
+#var2=$(grep -rnw 'resilio/folders/DripBox/QownNotes' -e $var1 | fzf) #| cut -d":" -f1,2 
+var2=$(egrep --recursive --with-filename --line-number --binary-files=without-match "$var1" 'resilio/folders/DripBox/QownNotes' | fzf)
 
-path=$(echo $var2 | cut -d":" -f1)
-line=$(echo $var2 | cut -d":" -f2)
+if [ -z "$var2" ]; then
+	echo "empty, quitting"
+else
+	path=$(echo $var2 | cut -d":" -f1)
+	line=$(echo $var2 | cut -d":" -f2)
 
-vimCmd="vim -u ~/.vim/.vimrc"
+	vimCmd="vim -u ~/.vim/.vimrc"
 
-eval $vimCmd +$line \"$path\"
+	eval $vimCmd +$line \"$path\"
+fi
