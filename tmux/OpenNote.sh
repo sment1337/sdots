26a459cc8b36342bc381ea2a84ccacd23b0e8a81
sment <sment1337@gmail.com>
2024-09-22 09:51:04 -0700

    Added the fzf preview same as in wsl branch

--- tmux/OpenNote.sh
+++ tmux/OpenNote.sh
@@ -5,7 +5,7 @@ echo 'type search keyword or regexp (/!\ Note: use -e ASDASD to do or search):'
 read var1
 
 #var2=$(grep -rnw 'resilio/folders/DripBox/QownNotes' -e $var1 | fzf) #| cut -d":" -f1,2 
-var2=$(egrep --recursive --with-filename --line-number --binary-files=without-match "$var1" '/home/sment/DripBox/QownNotes' | fzf)
+var2=$(egrep --recursive --with-filename --line-number --binary-files=without-match "$var1" '/home/sment/DripBox/QownNotes' | fzf --preview-window 'right,70%' --bind "ctrl-p:preview(sh ~/sdots/tmux/txtPreview.sh {}),ctrl-/:change-preview-window(hidden),pgdn:preview-page-down,pgup:preview-page-up")
 
 if [ -z "$var2" ]; then
 	echo "empty, quitting"
---
+++ tmux/txtPreview.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/zsh
+
+path=$(echo $1 | /usr/bin/cut -d":" -f1)
+line=$(echo $1 | /usr/bin/cut -d":" -f2)
+
+Cmd="cat "
+PageArgs="--line-range"
+SearchArgs="-p "
+
+eval $Cmd $PageArgs $line: \"$path\"
+#echo $Cmd $PageArgs$line  $SearchArgs $2 \"$path\"
