a069821fcbc8d04f5e4dd47ac86fa19be369e674
sment <sment1337@gmail.com>
2024-11-23 08:19:15 -0800

    Added conky config

--- lnkScript.sh
+++ lnkScript.sh
@@ -1,11 +1,12 @@
 rm -rf .vim .zshrc .tmux.conf .config
-printf '1\nY\n' | sudo pacman -S terminator i3-wm i3status xorg-xrandr picom polybar upower python-pip base-devel wget sddm nitrogen rofi
+printf '1\nY\n' | sudo pacman -S terminator i3-wm i3status xorg-xrandr picom polybar upower python-pip base-devel wget sddm nitrogen rofi conky
 sudo systemctl enable sddm.service
 git clone https://aur.archlinux.org/yay.git
 cd yay
 printf 'Y\nY\n' | makepkg -si
 cd ~
 printf 'Y\nN\nN\nY\n' | yay -S ttf-jetbrains-mono-nerd autojump sddm-theme-sugar-candy-git
+sudo pacman -S python-beautifulsoup4
 pipx install bs4
 ln -s sdots/.vim ./.vim
 ln -s sdots/.zshrc ./.zshrc
@@ -15,6 +16,7 @@ ln -s /home/sment/sdots/i3 .config/i3
 ln -s /home/sment/sdots/polybar .config/polybar
 ln -s /home/sment/sdots/nitrogen/ .config/nitrogen
 ln -s /home/sment/sdots/rofi/ .config/rofi
+ln -s /home/sment/sdots/conky/ .config/conky
 
 # Theme stuff. First make Sugar Candy Backgrounds folder open to write images
 sudo chmod -R 777 /usr/share/sddm/themes/Sugar-Candy/Backgrounds
