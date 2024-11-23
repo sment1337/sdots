aafb2c36f3a1e58c14c904d5147c6d0afa708b90
sment <sment1337@gmail.com>
2024-11-23 13:00:01 -0800

    Added the correct OpenNote and OpenPDF scripts with preview

--- tmux/OpenNote.sh
+++ tmux/OpenNote.sh
@@ -1,14 +1,34 @@
+##!/usr/bin/zsh
+#
+#echo 'type search keyword or regexp (/!\ Note: use -e ASDASD to do or search):'
+#
+#read var1
+#
+#var2=$(grep -rnw '/home/sment/DripBox/QownNotes' -e $var1 | fzf) #| cut -d":" -f1,2 
+#
+#path=$(echo $var2 | cut -d":" -f1)
+#line=$(echo $var2 | cut -d":" -f2)
+#
+#vimCmd="vim -u ~/.vim/.vimrc"
+#
+#eval $vimCmd +$line \"$path\"
+#
 #!/usr/bin/zsh
 
 echo 'type search keyword or regexp (/!\ Note: use -e ASDASD to do or search):'
 
 read var1
 
-var2=$(grep -rnw '/home/sment/DripBox/QownNotes' -e $var1 | fzf) #| cut -d":" -f1,2 
+#var2=$(egrep --recursive --with-filename --line-number --binary-files=without-match "$var1" 'resilio/folders/DripBox/QownNotes' | fzf)
+var2=$(egrep --recursive --with-filename --line-number --binary-files=without-match $var1 'DripBox/QownNotes' | fzf --preview-window 'right,70%' --bind "ctrl-p:preview(sh ~/sdots/tmux/previewNote.sh {} $var1),ctrl-/:change-preview-window(hidden),pgdn:preview-page-down,pgup:preview-page-up")
 
-path=$(echo $var2 | cut -d":" -f1)
-line=$(echo $var2 | cut -d":" -f2)
+if [ -z "$var2" ]; then
+	echo "empty, quitting"
+else
+	path=$(echo $var2 | cut -d":" -f1)
+	line=$(echo $var2 | cut -d":" -f2)
 
-vimCmd="vim -u ~/.vim/.vimrc"
+	vimCmd="vim -u ~/.vim/.vimrc"
 
-eval $vimCmd +$line \"$path\"
+	eval $vimCmd +$line \"$path\"
+fi
---
+++ tmux/preview.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/zsh
+
+path=$(echo $1 | cut -d":" -f1)
+line=$(echo $1 | cut -d":" -f2)
+
+Cmd="zathura -P "
+CmdArgs="-f "
+
+eval $Cmd $line $CmdArgs $2 \"$path\" 
