5cffbe6e32b16d550eab9c985cc99265916f3db7
sment <sment1337@gmail.com>
2026-06-12 01:14:14 +0300

    fixed so the preview script doesn't generate .swp files

--- tmux/OpenNote.sh
+++ tmux/OpenNote.sh
@@ -4,7 +4,15 @@ echo 'type search keyword or regexp (/!\ Note: use -e ASDASD to do or search):'
 
 read var1
 
-var2=$(grep -rnw ~/DripBox/QownNotes -e $var1 | fzf --preview-window 'right,40%' --bind "ctrl-p:preview(sh $HOME/sdots/tmux/preview.sh {}),ctrl-/:change-preview-window(hidden)")
+#var2=$(grep -rnw ~/DripBox/QownNotes -e $var1 | fzf --preview-window 'right,40%' --bind "ctrl-p:preview(sh $HOME/sdots/tmux/preview.sh {}),ctrl-z:change-preview-window(hidden)")
+var2=$(egrep -rnI \
+  --exclude-dir=__pycache__ \
+  --exclude-dir=site-packages \
+  --exclude-dir=.git \
+  --exclude-dir=venv \
+  --exclude-dir=.venv \
+  "$var1" ~/DripBox/QownNotes | fzf --preview-window 'right,40%' --bind "ctrl-p:preview(sh $HOME/sdots/tmux/preview.sh {}),ctrl-z:change-preview-window(hidden)")
+
 
 path=$(echo $var2 | cut -d":" -f1)
 line=$(echo $var2 | cut -d":" -f2)
--- tmux/preview.sh
+++ tmux/preview.sh
@@ -3,7 +3,7 @@
 path=$(echo $1 | cut -d":" -f1)
 line=$(echo $1 | cut -d":" -f2)
 
-Cmd="vim -u ~/.vim/.vimrc"
+Cmd="vim -n -u ~/.vim/.vimrc"
 CmdArgs="+"
 
 eval $Cmd $CmdArgs$line \"$path\"
