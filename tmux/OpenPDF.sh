f7fc3bd0f56514bcf4813ba554b448f0551bd7ce
sment <sment1337@gmail.com>
2026-06-12 12:03:02 +0300

    fixed the openPDF script to preselect directories

--- tmux/OpenPDF.sh
+++ tmux/OpenPDF.sh
@@ -1,14 +1,27 @@
 #!/usr/bin/zsh
 
+# Choose literature folder
+echo "Select literature folder:"
+echo "1) EE Design Books"
+echo "2) analog design"
+echo "3) All (entire Literature directory)"
+read choice
+
+if [[ $choice == "1" ]]; then
+    var1="$HOME/DripBox/iCloudDocs/Literature/EE Design Books"
+elif [[ $choice == "2" ]]; then
+    var1="$HOME/DripBox/iCloudDocs/Literature/analog design"
+elif [[ $choice == "3" ]]; then
+    var1="$HOME/DripBox/iCloudDocs/Literature"
+fi
+
 echo 'type search keyword or regexp:'
 
-read var1
+read var2
 
-var2=$(pdfgrep --recursive --page-number $var1 "$HOME/DripBox/iCloudDocs/Literature/EE Design Books" | fzf --preview-window 'right,1%' --bind "enter:preview(sh $HOME/sdots/tmux/pdfPreview.sh {} $var1),ctrl-/:change-preview-window(hidden)")
+var3=$(pdfgrep --recursive --page-number "$var2" "$var1" | fzf --preview-window 'right,1%,hidden' --bind "enter:preview(sh $HOME/sdots/tmux/pdfPreview.sh {} $var2),ctrl-/:change-preview-window(hidden)")
 
-path=$(echo $var2 | cut -d":" -f1)
-line=$(echo $var2 | cut -d":" -f2)
+path=$(echo "$var3" | cut -d":" -f1)
+line=$(echo "$var3" | cut -d":" -f2)
 
-Cmd="zathura -P "
-
-eval $Cmd $line \"$path\"
+zathura -P "$line" "$path"
